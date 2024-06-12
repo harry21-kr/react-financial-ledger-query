a67423c69797c4cc0bc54ff05270aad43a7fcb30
박원빈 <harry21@kakao.com>
2024-06-12 14:34:04 +0900

    (remove) redux store

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,20 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
-import store from "./store/store";
 
 const queryClient = new QueryClient();
 
 const Providers = () => {
   return (
-    <ReduxProvider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </ReduxProvider>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
--- src/store/paymentHistory/hooks.js
+++
@@ -1,5 +0,0 @@
-import { useSelector } from "react-redux";
-
-export const usePaymentHistoryList = () => {
-  return useSelector((state) => state.paymentHistory.history);
-};
